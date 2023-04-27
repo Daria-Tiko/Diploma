d8b6f3beb5e412411ec15c1e224111131c59f81f
Darya Tishchenko QA-41 <tdaniel@bk.ru>
2023-04-27 11:49:03 +0400

    Add SQLHelper

---
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -0,0 +1,59 @@
+package ru.netology.data;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+// docker-compose up
+// Запуск SUT с поддержкой для MySQL: java -jar artifacts/aqa-shop.jar -P:jdbc.url=jdbc:mysql://localhost:3306/app
+// Запуск SUT с поддержкой для Postgres: java -jar artifacts/aqa-shop.jar -P:jdbc.url=jdbc:postgresql://localhost:5432/app
+
+public class SQLHelper {
+    private static final QueryRunner runner = new QueryRunner();
+    private static final String url = System.getProperty("db.url");
+
+    // Подключение к БД
+    @SneakyThrows
+    public static Connection getConn(){
+        return DriverManager.getConnection(url, "app", "pass");
+    }
+
+    //Очистка данных
+    @SneakyThrows
+    public static void cleanDatabase() {
+        var connection = getConn();
+        runner.execute(connection, "DELETE FROM credit_request_entity");
+        runner.execute(connection, "DELETE FROM order_entity");
+        runner.execute(connection, "DELETE FROM payment_entity");
+    }
+
+    // Платёж дебетовой картой
+    @SneakyThrows
+    public static String getDebitPaymentStatus() {
+        String SqlStatus = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1";
+        return getResult(SqlStatus);
+    }
+
+    // Платёж кредитной картой
+    @SneakyThrows
+    public static String getCreditPaymentStatus() {
+        String SqlStatus = "SELECT status FROM credit_request_entity ORDER BY created DESC LIMIT 1";
+        return getResult(SqlStatus);
+    }
+
+    //
+    @SneakyThrows
+    private static String getResult(String query) {
+        String result = "";
+        var runner = new QueryRunner();
+        try (var connection = getConn()) {
+            result = runner.query(connection, query, new ScalarHandler<>());
+        }
+        return result;
+    }
+
+}
